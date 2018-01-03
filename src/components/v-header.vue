6081cc49dbd9a839e9f91257bdff9d4d89dc27cb
李雪姣5 <lixuejiao5@hikvision.com>
2018-01-03 20:56:58 +0800

    更新项目结构

---
+++ src/components/v-header.vue
@@ -0,0 +1,20 @@
+<template>
+    <section class="cv-header">
+        <ul>
+            <li>alert</li>
+            <li>modal</li>
+            <li>timePicker</li>
+            <li>carousel</li>
+            <li>timeLine</li>
+            <li>pagination</li>
+        </ul>
+    </section>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
